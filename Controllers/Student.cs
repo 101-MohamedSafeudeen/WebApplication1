28e9eecf77edf8dc96016c52bfe3b06880fee267
mohamedsafeudeen <mohammed.safeudeen@inceptionlabs.in>
2021-12-31 10:06:11 +0530

    Add project files.

---
+++ Controllers/Student.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class Student : Controller
+    {
+        public string Index()
+        {
+            return "Details About Student";
+
+        }
+        public string StudentId()
+        {
+            return "101";
+
+        }
+        public string StudentName()
+        {
+            return "Mohamed Safeudeen";
+
+        }
+    }
+}
